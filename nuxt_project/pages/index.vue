0180cbe53f18c38f481b30bbd4686a1cdc187ef0
KulchytskyiOleh <olegkakb@gmail.com>
2023-09-11 07:52:49 +0300

    first commit

---
+++ nuxt_project/pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem enim recusandae quod temporibus libero facilis cumque quasi. Omnis quia quo perspiciatis
+      necessitatibus soluta delectus fuga id. Dolor dicta ex asperiores! Quaerat laboriosam, nesciunt iste eos, rerum quas consequatur eveniet nam provident,
+      blanditiis praesentium doloribus ut! Animi, maxime. Quae, sunt quisquam quibusdam beatae libero, aspernatur reprehenderit quos esse fugit voluptates quam.
+      Error maxime eveniet numquam dolores voluptas molestias quaerat, delectus fuga laboriosam necessitatibus dolorum beatae vitae omnis quos alias! Ad sint,
+      facere provident fuga modi voluptatem dolorem eligendi eius a ut! Quisquam ut illo molestias dolores eligendi numquam veritatis, omnis tenetur incidunt
+      facere itaque at reiciendis cumque, sunt possimus ab corporis consequatur? Excepturi, quia et. Laborum eum beatae dolorum molestiae voluptatem? Quam unde
+      ut dicta nam assumenda aliquid debitis illo ab, nobis atque nemo, quasi corporis ratione voluptatibus, veniam perspiciatis. Voluptatum molestiae,
+      architecto sapiente non consequuntur alias fuga. Corrupti, tempora incidunt. Placeat nesciunt esse facilis, perferendis doloribus, expedita voluptate
+      inventore nobis consectetur impedit veniam velit. Nostrum quas sapiente placeat alias iste animi. Rem consequatur eos ipsam sapiente modi, veniam numquam.
+      Quaerat! Ad sed distinctio possimus vel magnam reprehenderit vero. Cum dolore, facere numquam debitis quibusdam tempora possimus amet distinctio dicta,
+      vitae quas vero molestias esse non, at voluptatum magni itaque in! Provident reiciendis quasi amet delectus blanditiis cumque itaque maiores, architecto
+      id distinctio doloribus corrupti! Vero unde id reprehenderit ducimus sapiente, ipsum recusandae quidem tempore harum. Possimus quibusdam hic culpa
+      consequuntur. Delectus praesentium in deleniti tempora maxime consequuntur saepe! Quam quaerat architecto ea alias asperiores? Praesentium consequuntur
+      rerum tenetur doloremque aspernatur, dolorem officiis cupiditate et ab natus sit necessitatibus! Suscipit, corrupti? Odio saepe voluptate porro, soluta
+      quidem veniam iure est dolore? Natus veritatis sint officia earum officiis. Aliquam iure aut soluta, illum accusantium explicabo, commodi corporis
+      perferendis in architecto magni ipsum? Vel quaerat consequuntur sequi aut culpa natus eos. Mollitia provident esse perferendis, animi, excepturi et
+      distinctio rerum eos quas incidunt obcaecati soluta maiores praesentium voluptatibus laudantium sint quaerat quia itaque? Dolorum aliquid illo inventore,
+      atque at laudantium accusantium reprehenderit expedita adipisci iure veritatis totam sequi quia nobis, sed id. Odit sapiente corrupti laudantium aliquid
+      veniam nulla ab corporis quos optio. Hic, eligendi perferendis. Distinctio eaque laboriosam consequatur vero iste quo unde, dignissimos excepturi
+      inventore omnis sequi ad, commodi magnam at non cum nihil quae possimus doloremque labore, laudantium voluptas perferendis. Cum vitae nulla odit
+      repudiandae corrupti. Corporis voluptates sequi praesentium! Ab odio facere labore ut architecto vel debitis ad mollitia nulla esse, enim autem cumque
+      modi velit laborum in aperiam? Eos consectetur similique, culpa qui illo placeat ab! Facere cum vitae autem, a necessitatibus possimus harum inventore
+      quae eligendi. Error praesentium corporis vel libero id, obcaecati minima inventore maiores! Ipsum? Laborum, odio tenetur! Ab consectetur porro provident
+      error nulla ex? Eligendi unde ipsum eius beatae quis ex similique dolores, magnam officiis quibusdam, saepe quo culpa porro. Praesentium omnis tempora ex!
+      Ipsum, voluptas voluptatem quisquam ex libero, error sapiente fugiat maxime neque ipsam architecto dolorum quod magnam explicabo deleniti laboriosam nemo
+      distinctio consectetur corrupti accusantium ipsa. Repellendus, nemo sunt. Sapiente, ut. Nulla inventore dolore animi, debitis, tenetur possimus quia quo
+      culpa ducimus soluta delectus repellendus. Quibusdam culpa animi sit, deleniti ratione, nihil dolore blanditiis saepe ea iure cumque corrupti officiis
+      quidem. Delectus magni possimus amet illo repudiandae adipisci itaque, molestiae a est sint esse hic sit dicta doloribus saepe, enim numquam animi,
+      cupiditate quibusdam temporibus architecto nisi facere deserunt nemo. Placeat. Ea nam incidunt tempora at saepe, asperiores architecto repellendus. Vel ea
+      obcaecati, consequatur ex quia quo, a provident totam blanditiis, amet maiores. Asperiores aut aperiam voluptatem laboriosam vero alias aliquid.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px;
+}
+</style>
